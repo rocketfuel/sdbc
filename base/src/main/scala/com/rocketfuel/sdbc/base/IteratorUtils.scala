d3b95ac757f533dffbc6b5a1870831e09c6189dd
Jeff Shaw <jshaw@rocketfuel.com>
2016-12-05 23:56:47 -0500

    simpler implementation

--- base/src/main/scala/com/rocketfuel/sdbc/base/IteratorUtils.scala
+++ base/src/main/scala/com/rocketfuel/sdbc/base/IteratorUtils.scala
@@ -2,18 +2,16 @@ package com.rocketfuel.sdbc.base
 
 import fs2.Stream
 import fs2.util.Async
-import fs2.util.syntax._
 
 private[sdbc] object IteratorUtils {
 
   def fromIterator[F[_], A](i: F[Iterator[A]])(implicit a: Async[F]): Stream[F, A] = {
     for {
       iterator <- Stream.eval(i)
-      elem <- Stream.unfoldEval[F, Iterator[A], A](iterator)(i =>
-        for {
-          hasNext <- a.delay[Boolean](i.hasNext)
-          elem <- if (hasNext) a.delay(Some((i.next(), i))) else a.pure(None)
-        } yield elem
+      elem <- Stream.unfold[F, Iterator[A], A](iterator)(i =>
+        if (i.hasNext)
+          Some((i.next, i))
+        else None
       )
     } yield elem
   }
